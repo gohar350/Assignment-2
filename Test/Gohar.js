5513a7b59477bedeac5a75015bffa96c8bbffda8
Gohar Butt <57588929+gohar350@users.noreply.github.com>
2022-11-14 23:07:18 +0500

    Nightwatch is working perfectly

--- Test/Gohar.js
+++ Test/Gohar.js
@@ -5,6 +5,12 @@ module.exports={
         browser.setValue('input[type=email','gohar350butt@gmail.com');
         browser.setValue('input[type=password','Butt9604');
         browser.click('button[type=submit]');
+        //Question
+        browser.click('#root > div > header > div > div.css-11x9xqg.e4w71dr2 > div.css-16y0tgk.e8mjkge0 > span:nth-child(1) > button')
+        browser.click('body > span.PopoverContainer.PopoverContainer--open.popover.tether-element.tether-element-attached-top.tether-target-attached-bottom.tether-element-attached-right.tether-target-attached-right.tether-enabled > span > div > div > div > ol > li:nth-child(1) > a > div > span')
+
+
+
         browser.click('#root > div > header > div > div.css-11x9xqg.e4w71dr2 > div.css-1rsu45y.e4w71dr0 > div > div > button');
         browser.pause(2000);
         browser.click('body > span > span > div > div > div > ol > li:nth-child(6) > div > div > span');
