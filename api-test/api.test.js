a6b497cc3de338e4c32b6a7ffdf97284d7fcb095
Gohar Butt <57588929+gohar350@users.noreply.github.com>
2022-11-24 14:10:30 +0500

    update Api test

--- api-test/api.test.js
+++ api-test/api.test.js
@@ -12,8 +12,28 @@ it('test case1', async()=>{
 })
 */
 
+jest.setTimeout(50000);
+
 const request = require('supertest')
 const app = require('http://localhost:3000/api/user/current')
+var token='';
+
+//Test1
+it('test1',async()=>{
+
+  let user ={
+    "username":"gohar350butt@gmail.com",
+    "password":"Butt9604"
+  }
+
+  const responce =await request.post('api/session').send(user);
+  expect(responce.status).toBe(200);
+  token=responce.body.id;
+  console.log(responce.body);
+})
+
+//Test2
+
 describe('Post Endpoints', () => {
   it('should create a new post', async () => {
     const res = await request(app)
